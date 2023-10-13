244407504c749a09d75fc0453013645397433156
KyanPepper <kyankks1@gmail.com>
2023-10-13 15:30:42 -0700

    Created Research post model and form

--- app/Controller/forms.py
+++ app/Controller/forms.py
@@ -1,6 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import  DataRequired, Length
+from app.Model.models import ResearchPost
 
-from app.Model.models import Post
 
+
+
+class ReasearchPostForm(FlaskForm):
+   title = StringField('Title', validators=[DataRequired()])
+   description = StringField('Title', validators=[DataRequired()])
+   qualifications = StringField('Title', validators=[DataRequired()])
+   major = SelectField('Sort',choices = [(1, 'Computer Science'), (2, 'Mechiancial Engineering'), (3,'Biology'),(4,'Electrical Engineering')])
+   submit =SubmitField('Refresh')
+
+    
