cd26bcd3932d191b490b8b5b594bac4615f0252d
KyanPepper <kyankks1@gmail.com>
2023-12-03 23:50:41 -0800

    Read me for setup and first test

---
+++ tests/tests_models.py
@@ -0,0 +1,38 @@
+import warnings
+warnings.filterwarnings("ignore")
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+from datetime import datetime, timedelta
+import unittest
+from app import create_app, db
+from app.Model.models import User
+from config import Config
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    ROOT_PATH = '..//'+basedir
+    
+class TestModels(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_password_hashing(self):
+        u = User(username='john', email='john.yates@wsu.edu')
+        u.set_password('covid')
+        self.assertFalse(u.get_password('flu'))
+        self.assertTrue(u.get_password('covid'))
+
+    
+if __name__ == '__main__':
+    unittest.main()
